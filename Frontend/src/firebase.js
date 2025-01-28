75770a8eb0b3c783d75e131f8fd6618dab9eb49d
ScriptifySolution <scriptifysolution30@gmail.com>
2025-01-29 04:39:41 +0530

    Update firebase.js

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDGwWG0pjBRcTmjl_5iNw9Cet44DJ8Vlwg",
   authDomain: "wisume-cc041.firebaseapp.com",
@@ -16,6 +12,5 @@ const firebaseConfig = {
   measurementId: "G-GLS3ZT3F2V"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
